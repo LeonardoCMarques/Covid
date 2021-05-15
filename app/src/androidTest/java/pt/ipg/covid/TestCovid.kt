7d13816a96bdc04171964ffbcc8c55993c251a11
Leonardo Marques <leonardo2000marques@gmail.com>
2021-05-15 23:18:47 +0100

    Initial commit

---
+++ app/src/androidTest/java/pt/ipg/covid/TestCovid.kt
@@ -0,0 +1,33 @@
+package pt.ipg.covid
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Before
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class TestBaseDados {
+    private fun getAppContext() = InstrumentationRegistry.getInstrumentation().targetContext
+
+    @Before
+    fun apagaBaseDados(){
+        getAppContext().deleteDatabase(BdCovidOpenHelper.NOME_BASE_DADOS)
+    }
+
+    @Test
+    fun consegueAbrirBaseDados(){
+        val dbOpenHelper = BdCovidOpenHelper(getAppContext())
+        val db = dbOpenHelper.readableDatabase
+        assert(db.isOpen)
+        db.close()
+    }
+}
---
+++ app/src/main/java/pt/ipg/covid/TabelaNumCasosCovid.kt
@@ -0,0 +1,58 @@
+package pt.ipg.covid
+
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaNumCasosCovid(db: SQLiteDatabase) {
+    private val db: SQLiteDatabase = db
+
+    fun cria(){
+        db.execSQL("CREATE TABLE " + NOME_TABELA + "(" +
+                BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                CAMPO_NOME + " TEXT NOT NULL, " +
+                CAMPO_IDADE+ " TEXT NOT NULL, " +
+                CAMPO_ID_NUM_CASOS_RECUPERADOS+ " INTEGER, " +
+                CAMPO_ID_NUM_VACINADOS + " INTEGER, " +
+                " FOREIGN KEY ("+ CAMPO_ID_NUM_CASOS_RECUPERADOS +") " +
+                " REFERENCES " + TabelaNumCasosRecuperados.NOME_TABELA +
+                " FOREIGN KEY ("+ CAMPO_ID_NUM_VACINADOS +") " +
+                " REFERENCES " + TabelaNumVacinados.NOME_TABELA +
+                ")"
+        )
+
+    }
+
+    fun insert(values: ContentValues): Long {
+        return db.insert(NOME_TABELA,null, values)
+    }
+
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int {
+        return db.update(NOME_TABELA, values, whereClause, whereArgs)
+    }
+
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(NOME_TABELA,whereClause, whereArgs)
+    }
+
+    fun query(
+        columns: Array<String>,
+        selection: String,
+        selectionArgs: Array<String>,
+        groupBy: String,
+        having: String,
+        orderBy: String
+    ): Cursor? {
+        return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
+    }
+
+    companion object{
+        const val NOME_TABELA = "casosCovid"
+        const val CAMPO_IDADE = "idade"
+        const val CAMPO_NOME = "nome"
+        const val CAMPO_ID_NUM_CASOS_RECUPERADOS = "id_num_casos_recuperados"
+        const val CAMPO_ID_NUM_VACINADOS = "id_num_vacinados"
+    }
+
+}
