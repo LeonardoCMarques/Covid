b9e7bdc9ad1729720205249e63ca8dfa0bf03705
Leonardo Marques <leonardo2000marques@gmail.com>
2021-07-17 17:43:04 +0100

    Implementado eliminar enfermeiro

--- app/src/main/java/pt/ipg/covid/EliminaEnfermeiroFragment.kt
+++ app/src/main/java/pt/ipg/covid/EliminaEnfermeiroFragment.kt
@@ -1,51 +1,85 @@
 package pt.ipg.covid
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.loader.app.LoaderManager
+import androidx.navigation.fragment.findNavController
+import pt.ipg.covid.DadosApp.Companion.activity
+
 class EliminaEnfermeiroFragment {
 
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var textViewEnfermeiro: TextView
+    private lateinit var textViewIdade: TextView
+    private lateinit var textViewSexo: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        DadosApp.fragment = this
+        (activity as MainActivity).menuAtual = R.menu.menu_elimina_enfermeiro
         return inflater.inflate(R.layout.fragment_elimina_enfermeiro, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EliminaLivroFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EliminaEnfermeiroFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textViewEnfermeiro = view.findViewById(R.id.textViewNomeEnfermeiro)
+        textViewIdade = view.findViewById(R.id.textViewIdade)
+        textViewSexo = view.findViewById(R.id.textViewSexo)
+
+        val enfermeiro = DadosApp.enfermeiroSelecionado!!
+        textViewEnfermeiro.setText(enfermeiro.nome)
+        textViewIdade.setText(enfermeiro.dataNascimento)
+        textViewSexo.setText(enfermeiro.sexo)
+    }
+
+    fun elimina() {
+        val uriEnfermeiro = Uri.withAppendedPath(
+            ContentProviderCovid.ENDERECO_TABELA_ENFERMEIRO,
+            DadosApp.enfermeiroSelecionado!!.id.toString()
+        )
+
+        val registos = activity?.contentResolver?.delete(
+            uriEnfermeiro,
+            null,
+            null
+        )
+
+        if (registos != 1) {
+            Toast.makeText(
+                requireContext(),
+                R.string.erro_eliminar_enfermeiro,
+                Toast.LENGTH_LONG
+            ).show()
+            return
+        }
+
+        Toast.makeText(
+            requireContext(),
+            R.string.enfermeiro_eliminado_sucesso,
+            Toast.LENGTH_LONG
+        ).show()
+        navegaListaEnfermeiro()
+    }
+
+    fun processaOpcaoMenu(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_confirma_eliminar_enfermeiro -> elimina()
+            R.id.action_cancelar_eliminar_enfermeiro -> navegaListaEnfermeiro()
+            else -> return false
+        }
+
+        return true
     }
 
 }
